74a7da368268e93b06546f763ce3b545b4108907
omatron <24454511+omatron@users.noreply.github.com>
2021-05-22 20:54:02 -0300

    jcp adicionado

--- earnings_parse.py
+++ earnings_parse.py
@@ -3,10 +3,11 @@ import csv
 import string
 
 # Clean File
-lista=[]
+lista_acoes=[]
+lista_jcp=[]
 
 
-with open('full.csv', encoding="utf8", errors="replace") as f, open('fixed.csv', 'w', encoding="utf8", errors="replace") as out:
+with open('../full.csv', encoding="utf8", errors="replace") as f, open('../fixed.csv', 'w', encoding="utf8", errors="replace") as out:
     reader = f.readlines()
     
     for line in reader:
@@ -14,13 +15,19 @@ with open('full.csv', encoding="utf8", errors="replace") as f, open('fixed.csv',
         ACOES=line.split(";")
         coluna=ACOES[2]
         #print(coluna)
-        find=re.findall('ACOES ([A-Z]{4}[0-9]{1,2})',coluna)
-        if find:
-            lista.append(find[0])
+        find_acoes=re.findall('ACOES ([A-Z]{4}[0-9]{1,2})',coluna)
+        find_jcp=re.findall(' BR([A-Z]{9}[0-9]{1,2})',coluna)
+        
+        if find_acoes:
+            lista_acoes.append(find_acoes[0])
+        if find_jcp:
+            lista_jcp.append(find_jcp[0])
     # Remove duplicados
-    mylist = list(dict.fromkeys(lista))
-    print(mylist)
-        # out.writelines(line)
+    lista_acoes_u = list(dict.fromkeys(lista_acoes))
+    print(lista_acoes_u)
+    lista_jcp_u = list(dict.fromkeys(lista_jcp))
+    print(lista_jcp_u)
+    
 
 f.close()
 
